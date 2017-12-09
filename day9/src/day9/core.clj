773e5789249276c82203f348b82587f07d19e62b
John Warwick <jwarwick@gmail.com>
2017-12-09 11:42:24 -0500

    Day 9, part 2

--- day9/src/day9/core.clj
+++ day9/src/day9/core.clj
@@ -10,18 +10,19 @@
   (loop [stream input
          depth 0
          total 0
+         garbage-count 0
          in-garbage false]
     (let [f (first stream)
           s (second stream)]
     (cond
-      (empty? stream) total
-      (and in-garbage (= f \!)) (recur (rest (rest stream)) depth total in-garbage)
-      (and in-garbage (= f \>)) (recur (rest stream) depth total false)
-      in-garbage (recur (rest stream) depth total in-garbage)
-      (= f \<) (recur (rest stream) depth total true)
-      (= f \{) (recur (rest stream) (inc depth) total in-garbage)
-      (= f \}) (recur (rest stream) (dec depth) (+ total depth) in-garbage)
-      :else (recur (rest stream) depth total in-garbage)
+      (empty? stream) [total garbage-count]
+      (and in-garbage (= f \!)) (recur (rest (rest stream)) depth total garbage-count in-garbage)
+      (and in-garbage (= f \>)) (recur (rest stream) depth total garbage-count false)
+      in-garbage (recur (rest stream) depth total (inc garbage-count) in-garbage)
+      (= f \<) (recur (rest stream) depth total garbage-count true)
+      (= f \{) (recur (rest stream) (inc depth) total garbage-count in-garbage)
+      (= f \}) (recur (rest stream) (dec depth) (+ total depth) garbage-count in-garbage)
+      :else (recur (rest stream) depth total garbage-count in-garbage)
       ))))
 
 (defn -main
@@ -34,5 +35,5 @@
                   slurp
                   string/trim)]
       (let [total-score (score input)]
-        (println "Part 1:" total-score)
-        (println "Part 2: TBD")))))
+        (println "Part 1:" (first total-score))
+        (println "Part 2:" (second total-score))))))
--- day9/test/day9/core_test.clj
+++ day9/test/day9/core_test.clj
@@ -3,11 +3,20 @@
             [day9.core :refer :all]))
 
 (deftest part1
-         (is (= (score "{}") 1))
-         (is (= (score "{{{}}}") 6))
-         (is (= (score "{{},{}}") 5))
-         (is (= (score "{{{},{},{{}}}}") 16))
-         (is (= (score "{<a>,<a>,<a>,<a>}") 1))
-         (is (= (score "{{<ab>},{<ab>},{<ab>},{<ab>}}") 9))
-         (is (= (score "{{<!!>},{<!!>},{<!!>},{<!!>}}") 9))
-         (is (= (score "{{<a!>},{<a!>},{<a!>},{<ab>}}") 3)))
+         (is (= (first (score "{}")) 1))
+         (is (= (first (score "{{{}}}")) 6))
+         (is (= (first (score "{{},{}}")) 5))
+         (is (= (first (score "{{{},{},{{}}}}")) 16))
+         (is (= (first (score "{<a>,<a>,<a>,<a>}")) 1))
+         (is (= (first (score "{{<ab>},{<ab>},{<ab>},{<ab>}}")) 9))
+         (is (= (first (score "{{<!!>},{<!!>},{<!!>},{<!!>}}")) 9))
+         (is (= (first (score "{{<a!>},{<a!>},{<a!>},{<ab>}}")) 3)))
+
+(deftest part2
+         (is (= (second (score "<>")) 0))
+         (is (= (second (score "<random characters>")) 17))
+         (is (= (second (score "<<<<>")) 3))
+         (is (= (second (score "<{!>}>")) 2))
+         (is (= (second (score "<!!>")) 0))
+         (is (= (second (score "<!!!>>")) 0))
+         (is (= (second (score "<{o\"i!a,<{i<a>")) 10)))
