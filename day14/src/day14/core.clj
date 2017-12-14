6b2edf9dc3d93184a976171a648bc8d222766db2
John Warwick <jwarwick@gmail.com>
2017-12-14 15:18:59 -0500

    Day 14, part 2

--- day14/src/day14/core.clj
+++ day14/src/day14/core.clj
@@ -5,24 +5,77 @@
     [day14.knothash :as knot])
   (:gen-class))
 
+(defn build-hashes
+  "Build the list of hashes that make up the grid"
+  [key-str grid-size]
+  (->> key-str
+    (repeat grid-size)
+    (map-indexed (fn [idx s] (str s "-" idx)))
+    (map knot/dense-hash)
+    (map #(BigInteger. % 16))))
+
 (defn used-squares
   "Calcuate the number of set bits in a 128x128 grid with the given key-string"
-  [key-str]
-  (let [grid-size 128
-        hashes (->> key-str
-                 (repeat grid-size)
-                 (map-indexed (fn [idx s] (str s "-" idx)))
-                 (map knot/dense-hash)
-                 (map #(BigInteger. % 16))
-                 (map #(.bitCount %))
-                 (reduce +)
-                 )]
-    hashes))
+  [hashes]
+  (->> hashes
+    (map #(.bitCount %))
+    (reduce +)))
+
+(defn set-bits
+  "Return a list of set bits in the BitInteger"
+  [number]
+  (for [x (range 128)
+        :when (.testBit number x)]
+    x))
+
+(defn make-bit-map
+  "Make a hashmap of the set bits in the integer"
+  [row number]
+  (->> number
+    (set-bits)
+    (map #(vector [row %] true))
+    (into {})
+    ))
+
+(defn neighbors
+  [[r c]]
+  (list [(dec r) c]
+        [r (inc c)]
+        [(inc r) c]
+        [r (dec c)]))
+
+(defn remove-region
+  [start-node start-bits]
+  (loop [queue (list start-node)
+         bits start-bits]
+    (if (empty? queue)
+      bits
+      (let [n (first queue)]
+        (recur
+          (if (contains? bits n)
+            (concat (rest queue) (neighbors n))
+            (rest queue))
+          (dissoc bits n))))))
+  
+(defn count-regions
+  "Count the number of connected components in the inputs"
+  [hashes]
+  (let [set-bits (->> hashes
+                   (map-indexed (fn [idx s] (make-bit-map idx s)))
+                   (apply merge)
+                   )]
+    (loop [region 0
+           bits set-bits]
+      (if (empty? bits)
+        region
+        (recur (inc region) (remove-region (first (keys bits)) bits))))))
 
 (defn -main
   "AOC Day 13 entrypoint"
   [& args]
   (let [input "hxtvlmkl"
-        used (used-squares input)]
+        hashes (build-hashes input 128)
+        used (used-squares hashes)
+        regions (count-regions hashes)]
     (println "Part 1:" used)
-    (println "Part 2: TBD")))
+    (println "Part 2:" regions)))
--- day14/test/day14/core_test.clj
+++ day14/test/day14/core_test.clj
@@ -3,5 +3,11 @@
             [day14.core :refer :all]))
 
 (deftest part1
-  (let [input "flqrgnkx"]
-    (is (= 8108 (used-squares input)))))
+  (let [input "flqrgnkx"
+        hashes (build-hashes input 128)]
+    (is (= 8108 (used-squares hashes)))))
+
+(deftest part2
+  (let [input "flqrgnkx"
+        hashes (build-hashes input 128)]
+    (is (= 1242 (count-regions hashes)))))
