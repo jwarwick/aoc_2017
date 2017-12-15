08679c5934531da1fbfecb734601c0146c836b39
John Warwick <jwarwick@gmail.com>
2017-12-15 14:45:55 -0500

    Day 15, part 2

--- day15/src/day15/core.clj
+++ day15/src/day15/core.clj
@@ -8,8 +8,10 @@
 (def factor-b (BigInteger. "48271"))
 (def divisor (BigInteger. "2147483647"))
 (def low16bits (BigInteger. "ffff" 16))
+(def mod-a (BigInteger. "4"))
+(def mod-b (BigInteger. "8"))
 
-(defn count-matches
+(defn count-matches-part1
   "Return number of low order 16-bit matches between two generators"
   [a-start b-start]
   (loop [step 0
@@ -29,11 +31,41 @@
           (println step next-a next-b new-matches))
        (recur (inc step) next-a next-b new-matches))))) 
 
+(defn generate-next-value
+  "Generate the next valid value"
+  [curr-start factor mod-value]
+  (loop [curr curr-start]
+    (let [next-val (.remainder (.multiply factor curr) divisor)]
+      (if (= 0 (.remainder next-val mod-value))
+        next-val
+        (recur next-val)))))
+
+(defn count-matches-part2
+  "Return number of low order 16-bit matches between two generators"
+  [a-start b-start]
+  (loop [step 0
+         a (BigInteger. (str a-start))
+         b (BigInteger. (str b-start))
+         matches 0]
+    (if (= step 5000000)
+      matches
+      (let [next-a (generate-next-value a factor-a mod-a)
+            low16-a (.and next-a low16bits)
+            next-b (generate-next-value b factor-b mod-b)
+            low16-b (.and next-b low16bits)
+            new-matches (if (= 0 (.compareTo low16-a low16-b))
+                          (inc matches)
+                          matches)]
+        (when (= 0 (mod step 100000))
+          (println step next-a next-b new-matches))
+       (recur (inc step) next-a next-b new-matches))))) 
+
 (defn -main
   "AOC Day 15 entrypoint"
   [& args]
   (let [a-start 883
         b-start 879
-        matches (count-matches a-start b-start)]
-    (println "Part 1:" matches)
-    (println "Part 2: TBD")))
+        ;; matches (count-matches-part1 a-start b-start)
+        matches (count-matches-part2 a-start b-start)]
+    ;; (println "Part 1:" matches)
+    (println "Part 2:" matches)))
--- day15/test/day15/core_test.clj
+++ day15/test/day15/core_test.clj
@@ -2,8 +2,14 @@
   (:require [clojure.test :refer :all]
             [day15.core :refer :all]))
 
-(deftest part1
+;; (deftest part1
+;;          (let [a-start 65
+;;                b-start 8921
+;;                matches (count-matches-part1 a-start b-start)]
+;;            (is (= 588 matches))))
+
+(deftest part2
          (let [a-start 65
                b-start 8921
-               matches (count-matches a-start b-start)]
-           (is (= 588 matches))))
+               matches (count-matches-part2 a-start b-start)]
+           (is (= 309 matches))))
