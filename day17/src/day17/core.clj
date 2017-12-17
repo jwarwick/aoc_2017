66d6cf92330186f2849abf1ec0f2acd03aa52bbd
John Warwick <jwarwick@gmail.com>
2017-12-17 14:30:41 -0500

    Day 17, part 2

--- day17/src/day17/core.clj
+++ day17/src/day17/core.clj
@@ -11,7 +11,8 @@
          curr-pos 0
          buffer (vector 0)]
     (if (empty? cycles)
-      (get buffer (inc curr-pos))
+      (let [len (count buffer)]
+        (get buffer (mod (inc curr-pos) len)))
       (let [len (count buffer)
             ^long new-pos (mod (+ curr-pos step-size) len)
             front (subvec buffer 0 (inc new-pos))
@@ -20,9 +21,25 @@
             new-buffer (vec (concat front (vector (inc new-val)) back))]
         (recur (rest cycles) (inc new-pos) new-buffer)))))
 
+(defn track-spinlock-zero
+  "Don't keep a buffer, just track when we would write to index 1"
+  [^long step-size num-cycles]
+  (loop [cycles (range num-cycles)
+         curr-pos 0
+         val-idx1 0]
+    (if (empty? cycles)
+      val-idx1
+      (let [^long new-val (first cycles)
+            ^long new-pos (mod (+ curr-pos step-size) (inc new-val))]
+      (recur (rest cycles) (inc new-pos) (if (= 0 new-pos) (inc new-val) val-idx1))))))
+      
 (defn -main
   "AOC Day 16 entrypoint"
   [& args]
   (let [step-size 371
-        num-cycles 2017]
-    (println "Part 1:" (next-spinlock-value step-size num-cycles))))
+        num-cycles 2017
+        num-cycles-part2 50000000
+        curr-based (next-spinlock-value step-size num-cycles)
+        zero-based (track-spinlock-zero step-size num-cycles-part2)]
+    (println "Part 1:" curr-based)
+    (println "Part 2:" zero-based)))
--- day17/test/day17/core_test.clj
+++ day17/test/day17/core_test.clj
@@ -4,5 +4,12 @@
 
 (deftest part1
          (let [step-size 3
-               num-cycles 2017]
-           (is (= 638 (next-spinlock-value step-size num-cycles)))))
+               num-cycles 2017
+               curr-based (next-spinlock-value step-size num-cycles)]
+           (is (= 638 curr-based))))
+
+(deftest part2
+         (let [step-size 3
+               num-cycles 2017
+               zero-based (track-spinlock-zero step-size num-cycles)]
+           (is (= 1226 zero-based))))
