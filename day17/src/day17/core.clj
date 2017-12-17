d7dbf287770fb8248987df7afbb985b302f8fe7b
John Warwick <jwarwick@gmail.com>
2017-12-17 14:50:33 -0500

    Trying a reduce-based solution for Part 2, not much faster

--- day17/src/day17/core.clj
+++ day17/src/day17/core.clj
@@ -21,18 +21,29 @@
             new-buffer (vec (concat front (vector (inc new-val)) back))]
         (recur (rest cycles) (inc new-pos) new-buffer)))))
 
+;; (defn track-spinlock-zero
+;;   "Don't keep a buffer, just track when we would write to index 1"
+;;   [^long step-size num-cycles]
+;;   (loop [cycles (range num-cycles)
+;;          curr-pos 0
+;;          val-idx1 0]
+;;     (if (empty? cycles)
+;;       val-idx1
+;;       (let [^long new-val (first cycles)
+;;             ^long new-pos (mod (+ curr-pos step-size) (inc new-val))]
+;;       (recur (rest cycles) (inc new-pos) (if (= 0 new-pos) (inc new-val) val-idx1))))))
+      
+(defn spinlock
+  "Test if we write to idx 0"
+  [^long step-size [^long curr-pos ^long val-idx1] ^long new-val]
+  (let [^long new-pos (mod (+ curr-pos step-size) (inc new-val))]
+    [(inc new-pos) (if (= 0 new-pos) (inc new-val) val-idx1)]))
+      
 (defn track-spinlock-zero
   "Don't keep a buffer, just track when we would write to index 1"
-  [^long step-size num-cycles]
-  (loop [cycles (range num-cycles)
-         curr-pos 0
-         val-idx1 0]
-    (if (empty? cycles)
-      val-idx1
-      (let [^long new-val (first cycles)
-            ^long new-pos (mod (+ curr-pos step-size) (inc new-val))]
-      (recur (rest cycles) (inc new-pos) (if (= 0 new-pos) (inc new-val) val-idx1))))))
-      
+  [^long step-size ^long num-cycles]
+  (last (reduce (partial spinlock step-size) [0 0] (range (inc num-cycles)))))
+
 (defn -main
   "AOC Day 16 entrypoint"
   [& args]
